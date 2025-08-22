97b3dff23f9a9296333a67a319e58b0efc349fe4
Oscar Casallas <oscarcasallas2004@gmail.com>
2025-08-22 14:21:25 -0500

    Initial commit: Zoo_animals

---
+++ src/main/java/com/mycompany/zoo_animals/Zoo_animals.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zoo_animals;
+
+/**
+ *
+ * @author HP Core i5
+ */
+public class Zoo_animals {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
